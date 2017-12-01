4300e2f842b5939331ec588e7e104d34eddef65f
Gasser Claude, INI-DEV-COR-NDC-WAC <Claude.Gasser@swisscom.com>
2017-12-01 16:45:57 +0100

    Upate ignoring .log data

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -5,6 +5,8 @@
 from confluent_kafka import Producer
 
 p = Producer({'bootstrap.servers': 'mybroker,mybroker2'})
-for data in some_data_source:
-    p.produce('mytopic', data.encode('utf-8'))
+
+#for data in some_data_source:
+    #p.produce('mytopic', data.encode('utf-8'))
+p.produce('mytopic', 'test')
 p.flush()
