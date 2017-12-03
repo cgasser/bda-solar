130245a3effcefa6ae0f1ca7a358ce35172c3797
Gasser Claude, INI-DEV-COR-NDC-WAC <Claude.Gasser@swisscom.com>
2017-12-03 10:08:57 +0100

    Add timestamp to message

--- src/kafka_producer_working_example.py
+++ src/kafka_producer_working_example.py
@@ -3,22 +3,20 @@
 # Thanks goes to the example from https://docs.confluent.io/4.0.0/clients/producer.html
 
 from confluent_kafka import Producer
+from datetime import datetime
 import socket
 import logging
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.DEBUG)
 
 topic = 'myTopic'
-
+message = "Testnachricht von " + str(datetime.now())
 
 conf = {'bootstrap.servers': "localhost",
         'client.id': socket.gethostname(),
         'default.topic.config': {'acks': '1'}}
-logging.info("Confluent Message config client.id: " + socket.gethostname())
-
-producer = Producer(conf)
-
 
 def acked(err, msg):
     if err is not None:
@@ -26,10 +24,16 @@ def acked(err, msg):
     else:
         print("Message produced: %s" % (str(msg)))
 
-logging.info("Send Message: " + str('Dies ist eine Testnachricht'))
-producer.produce(topic, str('Dies ist eine Testnachricht'), callback=acked)
+
+if __name__ == "__main__":
+
+    logging.info("Confluent Message config client.id: " + socket.gethostname())
+    producer = Producer(conf)
+
+    logging.info("Send Message: " + str(message))
+    producer.produce(topic, str(message), callback=acked)
 
 
-# Wait up to 1 second for events. Callbacks will be invoked during
-# this method call if the message is acknowledged.
-producer.poll(1)
+    # Wait up to 1 second for events. Callbacks will be invoked during
+    # this method call if the message is acknowledged.
+    producer.poll(1)
