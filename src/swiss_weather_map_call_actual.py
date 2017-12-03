8ad1358c350c5d3ac8cb02bfb59fca62237ea546
Gasser Claude, INI-DEV-COR-NDC-WAC <Claude.Gasser@swisscom.com>
2017-12-03 11:18:40 +0100

    Include Kafka producer topic swiss_weather

--- src/swiss_weather_map_call_actual.py
+++ src/swiss_weather_map_call_actual.py
@@ -6,7 +6,8 @@ import json
 import time
 import requests
 import logging
-
+from confluent_kafka import Producer
+import socket
 
 def open_weather_call(stationId,epoch_time):
     conn = http.client.HTTPConnection("")
@@ -16,9 +17,34 @@ def open_weather_call(stationId,epoch_time):
     data = r.json()  # This will return entire content.
     #data['timestamp_collect'] = epoch_time
     logging.debug(data)
-    with open('/home/claude/repo/bda-solar/data/data_weather/open_swiss_weather_'+ str(stationId) +'_' + epoch_time + '.json', 'w', encoding='utf-8') as outfile:
-        json.dump(data, outfile, indent=4, ensure_ascii=False)
     conn.close()
+    return data
+
+
+def kafka_produce(data):
+    topic = 'swiss_weather'
+    conf = {'bootstrap.servers': "localhost",
+            'client.id': socket.gethostname(),
+            'default.topic.config': {'acks': '1'}}
+
+    logging.info("Confluent Message config client.id: " + socket.gethostname())
+    producer = Producer(conf)
+
+    logging.info("Send Message to topic : " + str(topic))
+    logging.debug("Write Message json" + str(data))
+    producer.produce(topic, str(data), callback=acked)
+
+    # Wait up to 1 second for events. Callbacks will be invoked during
+    # this method call if the message is acknowledged.
+    producer.poll(1)
+
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename='/home/claude/repo/bda-solar/log/solar_requests.log',
@@ -26,7 +52,14 @@ if __name__ == "__main__":
                         level=logging.DEBUG)
     epoch_time_now = str(round(time.time()))
     logging.info("Start API call Open Swiss Weather Map at Time: " + epoch_time_now)
+
+    #Call open Weather api to get Data
     stationIdEinsiedeln = "EIN"
+    weather_data = open_weather_call(stationIdEinsiedeln,epoch_time_now)
 
-    open_weather_call(stationIdEinsiedeln,epoch_time_now)
+    #Write to local store
+    with open('/home/claude/repo/bda-solar/data/data_weather/open_swiss_weather_'+ str(stationIdEinsiedeln) +'_' + epoch_time_now + '.json', 'w', encoding='utf-8') as outfile:
+        json.dump(weather_data, outfile, indent=4, ensure_ascii=False)
 
+    #Write to KAFKA
+    kafka_produce(weather_data)
