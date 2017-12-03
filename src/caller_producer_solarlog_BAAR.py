e6f8d2a52fd19491a85e7ec6fde2eb59fb137424
Gasser Claude, INI-DEV-COR-NDC-WAC <Claude.Gasser@swisscom.com>
2017-12-03 12:10:03 +0100

    Renamed folder.

--- src/caller_producer_solarlog_BAAR.py
+++ src/caller_producer_solarlog_BAAR.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     solar_data = solar_log_call(epoch_time_now)
 
     # Write to local store
-    path = os.path.realpath('../data/data_timestamp')
+    path = os.path.realpath('../data/data_solarlog')
     logging.info("Write JSON to : " + path)
     with open(path + '/pfadibaar_solarlog_' + str(pfadheimBaarCID) + '_' + epoch_time_now + '.json', 'w', encoding='utf-8') as outfile:
         json.dump(solar_data, outfile, indent=4, ensure_ascii=False)
--- src/caller_producer_swiss_weather_map_EIN.py
+++ src/caller_producer_swiss_weather_map_EIN.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ### lightweight open weather map api caller to test if this can be used to gatter data_weather
 
-import os, sys
+import os
 import http.client
 import json
 import time
@@ -11,13 +11,12 @@ from confluent_kafka import Producer
 import socket
 
 
-def open_weather_call(stationId,epoch_time):
+def open_weather_call(stationId):
     conn = http.client.HTTPConnection("")
-    r = requests.get("http://opendata.netcetera.com:80/smn/smn/"+ str(stationId))
+    r = requests.get("http://opendata.netcetera.com:80/smn/smn/" + str(stationId))
     logging.info("Response: " + str(r.status_code) + " " + r.reason)
 
     data = r.json()  # This will return entire content.
-    #data['timestamp_collect'] = epoch_time
     logging.debug(data)
     conn.close()
     return data
@@ -32,8 +31,8 @@ def kafka_produce(data):
     logging.info("Confluent Message config client.id: " + socket.gethostname())
     producer = Producer(conf)
 
-    logging.info("Send Message to topic : " + str(topic))
-    logging.debug("Write Message json" + str(data))
+    logging.info("Send message to topic : " + str(topic))
+    logging.debug("Write message json" + str(data))
     producer.produce(topic, str(data), callback=acked)
 
     # Wait up to 1 second for events. Callbacks will be invoked during
@@ -51,19 +50,19 @@ def acked(err, msg):
 if __name__ == "__main__":
     logging.basicConfig(filename='/var/log/swiss_weather.log',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.DEBUG)
+                        level=logging.INFO)
     epoch_time_now = str(round(time.time()))
     logging.info("Start API call Open Swiss Weather Map at Time: " + epoch_time_now)
 
-    #Call open Weather api to get Data
+    # Call open Weather api to get Data
     stationIdEinsiedeln = "EIN"
     weather_data = open_weather_call(stationIdEinsiedeln,epoch_time_now)
 
-    #Write to local store
-    path = os.path.realpath('../data/data_weather')
+    # Write to local store
+    path = os.path.realpath('../data/data_swiss_weather')
     logging.info("Write jsno to : " + path)
     with open(path + '/open_swiss_weather_'+ str(stationIdEinsiedeln) +'_' + epoch_time_now + '.json', 'w', encoding='utf-8') as outfile:
         json.dump(weather_data, outfile, indent=4, ensure_ascii=False)
 
-    #Write to KAFKA
+    # Write to KAFKA
     kafka_produce(weather_data)
