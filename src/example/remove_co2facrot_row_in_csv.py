32ef31a18f7dd138292706d22ae80cf064b5fb20
Gasser Claude, INI-DEV-COR-NDC-WAC <Claude.Gasser@swisscom.com>
2018-02-23 21:58:07 +0100

    Ohne Index.

--- src/example/remove_co2facrot_row_in_csv.py
+++ src/example/remove_co2facrot_row_in_csv.py
@@ -10,7 +10,7 @@ path_temp = '/home/bda/temp'
 client = InsecureClient('http://nh-01.ip-plus.net:50070', user='hdfs')
 allFiles = sorted(glob.glob(path + "/*.csv"))
 for file_ in allFiles:
-    df = pd.read_csv(file_, delimiter='\t')
+    df = pd.read_csv(file_, index_col=None, delimiter='\t')
     #print(df)
     if 'co2factor' in df.columns:
         del df['co2factor']
